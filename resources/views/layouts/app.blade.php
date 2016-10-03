7fdbf28072da1726ad49dad90179689e588428bd
Poltroneando <poltroneando@gmail.com>
2016-10-02 22:57:14 -0300

    ic0n3

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <title>Poltroneando</title>
-    <link rel="shortcut icon" type="image/png" href="icons/favicon.png"/>
+    <link rel="shortcut icon" href="{{{ asset('icons/favicon.png') }}}"/>
 
     <!-- Fonts -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css" integrity="sha384-XdYbMnZ/QjLh6iI4ogqCTaIjrFk87ip+ekIjefZch0Y+PvJ8CDYtEs1ipDmPorQ+" crossorigin="anonymous">
