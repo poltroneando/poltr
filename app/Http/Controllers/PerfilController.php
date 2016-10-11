e7077785b5759983c59c09ec88008a06f9819935
Poltroneando <poltroneando@gmail.com>
2016-10-11 00:11:13 -0300

    comit perfil 3

--- app/Http/Controllers/PerfilController.php
+++ app/Http/Controllers/PerfilController.php
@@ -13,4 +13,16 @@ class PerfilController extends Controller
     public function profile(){
         return view('profile/index', array('user' => Auth::user()));
     }
+     public function update_avatar(Request $request){
+    	// Handle the user upload of avatar
+    	if($request->hasFile('avatar')){
+    		$avatar = $request->file('avatar');
+    		$filename = time() . '.' . $avatar->getClientOriginalExtension();
+    		Image::make($avatar)->resize(300, 300)->save( public_path('./uploads/avatars/' . $filename ) );
+    		$user = Auth::user();
+    		$user->avatar = $filename;
+    		$user->save();
+    	}
+    	return view('profile/index', array('user' => Auth::user()) );
+    }
 }
