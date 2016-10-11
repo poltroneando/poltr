f9b0150091e07d87741bd0e67ffd724085ff7a73
Poltroneando <poltroneando@gmail.com>
2016-10-10 21:48:52 -0300

    comit perfil 2

--- public/js/main.js
+++ public/js/main.js
@@ -1,12 +1,18 @@
 $(document).ready(function(){
 	$("#btn-login").click(function(){
+		if ($(this).hasClass('active'))
+		{} else{
 		$(this).addClass('active');
 		$("#btn-cadast").removeClass('active');
 		$("form").toggleClass("form-login-hide");
+		}
 	});
 	$("#btn-cadast").click(function(){
+		if ($(this).hasClass('active'))
+		{} else{
 		$(this).addClass('active');
 		$("#btn-login").removeClass('active');
 		$("form").toggleClass("form-login-hide");
+		}
 	});
 });
