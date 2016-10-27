66ff0f250b21f9409ee709d0d25eaf429497a2c0
Poltroneando <poltroneando@gmail.com>
2016-10-27 20:37:20 -0200

    comit 27out-1

---
+++ app/Filme.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Filme extends Model
+{
+    protected $table = 'titulo';
+    protected $primaryKey = 'id_titulo';
+    public $timestamps = false;
+    //
+}
