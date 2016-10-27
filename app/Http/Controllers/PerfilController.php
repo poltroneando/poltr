95afc543bf1225f6c74910d8dc6b7a8beaebe16b
Poltroneando <poltroneando@gmail.com>
2016-10-27 21:35:22 -0200

    comit 27out-2

--- app/Http/Controllers/PerfilController.php
+++ app/Http/Controllers/PerfilController.php
@@ -11,6 +11,11 @@ use App\Http\Requests;
 class PerfilController extends Controller
 {
     //
+	public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     public function profile(){
         return view('profile/index', array('user' => Auth::user()));
     }
