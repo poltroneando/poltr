d61d05a44e9e5b6d96b0bb16a22c7b77233de70c
Poltroneando <poltroneando@gmail.com>
2016-10-30 22:47:09 -0200

    comit 30out-3

--- resources/views/genero.blade.php
+++ resources/views/genero.blade.php
@@ -4,12 +4,12 @@
 <div class="container">
     <div class="row">
         <div class"col-md-8 col-md-offset-2">
-            <h1>Gêneros</h1>            
+            <h3>Gêneros</h3>            
         </div>        
     </div>    
     <div class="row">
         @foreach($generos as $genero)   
-        <div class="col-xs-6 col-md-3">
+        <div class="col-xs-6 col-sm-3">
             <a href="">{{ $genero->descricao }}</a>
         </div>
         @endforeach
