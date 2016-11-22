338b18da2f27a7e5e2ae58feffce4cb3a8cb1d22
Poltroneando <poltroneando@gmail.com>
2016-11-22 00:26:44 -0200

    comit 22nov-01

--- app/Http/Controllers/FilmesController.php
+++ app/Http/Controllers/FilmesController.php
@@ -5,11 +5,12 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
-
+use GuzzleHttp\Client;
 class FilmesController extends Controller
 {
     public function show(){
-        //$generos = Genero::all();
-        return view('titulos/index');
+        $client = new Client();
+        $filmes = $client->get('http://www.omdbapi.com/');
+        return view('titulos/index',compact('filmes'));
     }
 }
--- resources/views/titulos/index.blade.php
+++ resources/views/titulos/index.blade.php
@@ -3,11 +3,11 @@
 <div class="container-fluid text-center">
   <div class="row content">
     <div class="col-sm-8 col-sm-offset-2 text-left">
-      <h1>Welcome</h1>
+      @foreach($filmes as $filme)   
+      <h4>Welcome</h4>
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
       <hr>
-      <h3>Test</h3>
-      <p>Lorem ipsum...</p>
+      @endforeach
     </div>
   </div>
 </div>
