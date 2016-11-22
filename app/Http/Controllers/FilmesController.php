119300efaa41947cde12023683f7c56b8f43b0bf
Poltroneando <poltroneando@gmail.com>
2016-11-22 18:32:01 -0200

    comit 22nov-05.4

--- app/Http/Controllers/FilmesController.php
+++ app/Http/Controllers/FilmesController.php
@@ -16,6 +16,6 @@ class FilmesController extends Controller
         ]);
         $filmes = json_decode($apirequest->getBody()->getContents());
         //return view('titulos/index',compact('filmes'));
-        return var_dump($apirequest->getBody()->getContents());
+        return var_dump($apirequest);
     }
 }
