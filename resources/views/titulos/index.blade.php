57dd43563fee6f8c1c16f6b01c38889c04951583
Poltroneando <poltroneando@gmail.com>
2016-11-22 20:38:47 -0200

    comit 22nov-08.9

--- resources/views/titulos/index.blade.php
+++ resources/views/titulos/index.blade.php
@@ -3,7 +3,9 @@
 <div class="container-fluid text-center">
   <div class="row content">
     <div class="col-sm-8 col-sm-offset-2 text-left">
-      @foreach($filmes as $filme)   
+      {{var_dump($filmes)}}
+      @foreach($filmes as $filme)
+      {{var_dump($filme) }}   
       <h4>{{ $filme->title }}1</h4>
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
       <hr>
