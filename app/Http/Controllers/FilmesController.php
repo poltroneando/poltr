84398802f48c7bf556f7a7a7259b63b6153c49dd
Poltroneando <poltroneando@gmail.com>
2016-11-22 19:17:26 -0200

    comit 22nov-07

--- app/Http/Controllers/FilmesController.php
+++ app/Http/Controllers/FilmesController.php
@@ -9,9 +9,10 @@ use GuzzleHttp\Client;
 class FilmesController extends Controller
 {
     public function show(){
-        $client = new Client();
-        $apirequest = $client->request('GET','http://www.omdbapi.com/?r=json&s=toy story&type=movie');
-        $filmes = json_decode($apirequest->getBody()->getContents());
+        //$client = new Client();
+        //$apirequest = $client->request('GET','http://www.omdbapi.com/?r=json&s=toy story&type=movie');
+        $apirequest = json_decode(file_get_contents('http://www.omdbapi.com/?r=json&s=toy story&type=movie'));
+        //$filmes = json_decode($apirequest->getBody()->getContents());
         //return view('titulos/index',compact('filmes'));
         return var_dump($apirequest);
     }
