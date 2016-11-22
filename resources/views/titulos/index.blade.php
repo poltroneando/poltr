0ed394894393880dbc50ee5a2cce2c67bd4e0023
Poltroneando <poltroneando@gmail.com>
2016-11-22 20:41:41 -0200

    comit 22nov-09.1

--- resources/views/titulos/index.blade.php
+++ resources/views/titulos/index.blade.php
@@ -7,7 +7,7 @@
       @foreach($filmes as $filme)
       {{var_dump($filme) }}   
       <h4>{{ $filme->title }}1</h4>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+      <p>Lorem ipsum .</p>
       <hr>
       @endforeach
     </div>
