1e1390797b5556b6248b8093af3c8bdd1ba1145b
Poltroneando <poltroneando@gmail.com>
2016-11-22 12:54:23 -0200

    comit 22nov-04

--- resources/views/titulos/index.blade.php
+++ resources/views/titulos/index.blade.php
@@ -4,7 +4,7 @@
   <div class="row content">
     <div class="col-sm-8 col-sm-offset-2 text-left">
       @foreach($filmes as $filme)   
-      <h4>{{ $filme->title }}</h4>
+      <h4>{{ $filme->title }}1</h4>
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
       <hr>
       @endforeach
