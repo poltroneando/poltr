b9de8d0a23eb2001814a4d5481740ee3da0613ce
Poltroneando <poltroneando@gmail.com>
2016-11-22 19:42:30 -0200

    comit 22nov-07.2

--- app/Http/Controllers/FilmesController.php
+++ app/Http/Controllers/FilmesController.php
@@ -11,7 +11,14 @@ class FilmesController extends Controller
     public function show(){
         //$client = new Client();
         //$apirequest = $client->request('GET','http://www.omdbapi.com/?r=json&s=toy story&type=movie');
-        $apirequest = json_decode(file_get_contents('http://www.omdbapi.com/?r=json&s=toy story&type=movie'),true);
+        //$apirequest = json_decode(file_get_contents('http://www.omdbapi.com/?r=json&s=toy story&type=movie'));
+        $curl_handle=curl_init();
+        curl_setopt($curl_handle, CURLOPT_URL,'http://www.omdbapi.com/?r=json&s=toy story&type=movie');
+        curl_setopt($curl_handle, CURLOPT_CONNECTTIMEOUT, 2);
+        curl_setopt($curl_handle, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($curl_handle, CURLOPT_USERAGENT, 'Poltroneando');
+        $apirequest = curl_exec($curl_handle);
+        curl_close($curl_handle);
         //$filmes = json_decode($apirequest->getBody()->getContents());
         //return view('titulos/index',compact('filmes'));
         return var_dump($apirequest);
